076d7601f95427cdba1061bf7d10180d08610c56
Emanuele Pace <emanuele.10@outlook.it>
2020-06-01 16:23:42 +0100

    Refactor MVC

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
+require './lib/bookmark'
+
 
 class BookmarkManager < Sinatra::Base
+    get '/' do
+      'Bookmark Manager'
+    end
 
-get '/' do
-  'Bookmark Manager'
-end
+    get '/bookmarks' do
+      @bookmarks = Bookmark.all
+      erb :bookmarks
+    end
 
+    run! if app_file == $0
 
-run! if app_file == $0
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,21 @@
-feature 'Viewing bookmarks' do
-  feature 'visiting the homepage' do
-    scenario 'visit the index page' do
-    visit('/')
-    expect(page).to have_content "Bookmark Manager"
-  end
+# feature 'Viewing bookmarks' do
+#   feature 'visiting the homepage' do
+#     scenario 'visit the index page' do
+#     visit('/')
+#     expect(page).to have_content "Bookmark Manager"
+#   end
+#  end
+# end
+
+feature 'Test bookmark route' do
+  scenario 'Visit bookmarkroute' do
+    visit('/bookmarks')
+      expect(page).to have_content "http://www.makersacademy.com"
+      expect(page).to have_content "http://www.destroyallsoftware.com"
+      expect(page).to have_content "http://www.google.com"
+ end
 end
-end
+
+
+# Write a failing feature test for viewing bookmarks
+# at the /bookmarks route.
